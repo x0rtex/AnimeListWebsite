946d7574f3edcb99269e08dd776f216f41a8222b
Alekss <x0rtex420@gmail.com>
2025-05-13 20:41:33 +0100

    Added placeholder code to my-list component

--- src/app/pages/my-list/my-list.component.ts
+++ src/app/pages/my-list/my-list.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AnimePaginationService} from '../../services/anime-pagination.service';
+import {NgForOf, NgIf, NgOptimizedImage, SlicePipe} from '@angular/common';
 
 @Component({
   selector: 'app-my-list',
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    SlicePipe,
+    NgOptimizedImage
+  ],
   templateUrl: './my-list.component.html',
   styleUrl: './my-list.component.scss'
 })
-export class MyListComponent {
+export class MyListComponent implements OnInit {
+  myList: any[] = [];
+  expandedAnimeId: number | null = null;
+  loading: boolean = false;
+  error: string = '';
+  currentPage: number = 1;
+  totalPages: number = 1;
+  perPage: number = 18;
 
+  constructor(private pagination: AnimePaginationService) {}
+
+  ngOnInit(): void {
+    this.loadMyList();
+  }
+
+  loadMyList(page: number = 1): void {
+    this.pagination.fetchPaginated(
+      "TODO",
+      {page, perPage: this.perPage},
+      (data: any[]): any[] => this.myList = data,
+      (current: number, total: number): void => {
+        this.currentPage = current;
+        this.totalPages = total;
+      },
+      (loading: boolean): boolean => this.loading = loading,
+      (error: string): string => this.error = error
+    );
+  }
 }
